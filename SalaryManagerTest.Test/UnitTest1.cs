1c650a0c7ed5e5608b17e6559a6a81870a11128b
OKAJIMA <okajima100@gmail.com>
2023-02-23 22:04:02 +0900

    テスト用SQLを削除

--- SalaryManagerTest.Test/UnitTest1.cs
+++ SalaryManagerTest.Test/UnitTest1.cs
@@ -13,46 +13,6 @@ namespace SalaryManagerTest.Test
         [TestMethod]
         public void TestMethod1()
         {
-            /*try
-            {
-                string sql = @"
-SELECT Id, 
-YearMonth, 
-BasicSalary, 
-ExecutiveAllowance, 
-DependencyAllowance, 
-OvertimeAllowance, 
-DaysoffIncreased, 
-NightworkIncreased ,
-HousingAllowance,
-LateAbsent,
-TransportationExpenses,
-SpecialAllowance,
-SpareAllowance,
-Remarks,
-TotalSalary,
-TotalDeductedSalary
-FROM Allowance";
-
-                using (var connection = new SQLiteConnection(ConnectionString))
-                {
-                    using (var command = new SQLiteCommand(sql, connection))
-                    {
-                        connection.Open();
-
-                        using (var reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                //result.Add(createEntity(reader));
-                            }
-                        }
-                    }
-                }
-            }catch (Exception ex)
-            {
-
-            }*/
 
         }
     }
