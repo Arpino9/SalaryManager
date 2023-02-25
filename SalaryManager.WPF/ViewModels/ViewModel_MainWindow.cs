b88c4f3751c396eec8670d03343e0e761e06700e
OKAJIMA <okajima100@gmail.com>
2023-02-25 15:25:34 +0900

    ViewModel追加

---
+++ SalaryManager.WPF/ViewModels/ViewModel_MainWindow.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace SalaryManager.WPF.ViewModels
+{
+    public sealed class ViewModel_MainWindow
+    {
+        #region Property Changed
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var d = PropertyChanged;
+            d?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        public ViewModel_MainWindow()
+        {
+            
+        }
+
+        private string _priceUpDown;
+
+        /// <summary>
+        /// 基本給
+        /// </summary>
+        public string PriceUpdown
+        {
+            get => this._priceUpDown;
+            set
+            {
+                this._priceUpDown = value;
+                this.RaisePropertyChanged();
+            }
+        }
+    }
+}
