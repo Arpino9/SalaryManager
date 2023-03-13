3bbe97798f1e9be0231662e7dabc7fb5527ef241
OKAJIMA <okajima100@gmail.com>
2023-03-13 22:25:47 +0900

    SQLite接続クラスのXMXLコメント

--- SalaryManager.Infrastructure/SQLite/SQLiteHelper.cs
+++ SalaryManager.Infrastructure/SQLite/SQLiteHelper.cs
@@ -20,30 +20,35 @@ namespace SalaryManager.Infrastructure.SQLite
             var projectName = Assembly.GetExecutingAssembly().GetName().Name;
             var sqlitePath  = $"{FilePath.GetSolutionPath()}\\{projectName}\\{FilePath.GetSolutionName()}.db";
 
-            return $"Data Source={sqlitePath};Version={SQLiteHelper.Version};";
+            return ($"Data Source={sqlitePath};Version={SQLiteHelper.Version};");
         }
 
+        /// <summary>
+        /// クエリ実行
+        /// </summary>
+        /// <typeparam name="T">型パラメーター</typeparam>
+        /// <param name="sql">SQL文</param>
+        /// <param name="createEntity">読み込まれるエンティティ</param>
+        /// <returns>レコード(複数行)</returns>
+        /// <remarks>
+        /// 読み込まれるエンティティを引数で指定する。
+        /// </remarks>
         internal static IReadOnlyList<T> Query<T>(
             string sql,
             Func<SQLiteDataReader, T> createEntity)
         {
             var result = new List<T>();
 
-            var projectName = Assembly.GetExecutingAssembly().GetName().Name;
-            var sqlitePath = $"{FilePath.GetSolutionPath()}\\{projectName}\\{FilePath.GetSolutionName()}.db";
-            
             using (var connection = new SQLiteConnection(SQLiteHelper.GetConnectionString()))
+            using (var command = new SQLiteCommand(sql, connection))
             {
-                using (var command = new SQLiteCommand(sql, connection))
-                {
-                    connection.Open();
+                connection.Open();
 
-                    using (var reader = command.ExecuteReader())
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
                     {
-                        while (reader.Read())
-                        {
-                            result.Add(createEntity(reader));
-                        }
+                        result.Add(createEntity(reader));
                     }
                 }
             }
@@ -51,6 +56,17 @@ namespace SalaryManager.Infrastructure.SQLite
             return result;
         }
 
+        /// <summary>
+        /// クエリ実行
+        /// </summary>
+        /// <typeparam name="T">型パラメーター</typeparam>
+        /// <param name="sql">SQL文</param>
+        /// <param name="createEntity">読み込まれるエンティティ</param>
+        /// <param name="nullEntity">nullエンティティ</param>
+        /// <returns>レコード(単数行)</returns>
+        /// <remarks>
+        /// パラメータがない場合に使う。
+        /// </remarks>
         internal static T QuerySingle<T>(
             string sql,
             Func<SQLiteDataReader, T> createEntity,
@@ -59,14 +75,26 @@ namespace SalaryManager.Infrastructure.SQLite
             return QuerySingle<T>(sql, null, createEntity, nullEntity);
         }
 
+        /// <summary>
+        /// クエリ実行
+        /// </summary>
+        /// <typeparam name="T">型パラメーター</typeparam>
+        /// <param name="sql">SQL文</param>
+        /// <param name="parameters">パラメーター</param>
+        /// <param name="createEntity">読み込まれるエンティティ</param>
+        /// <param name="nullEntity">nullエンティティ</param>
+        /// <returns>レコード(単数行)</returns>
+        /// <remarks>
+        /// 該当する1レコードのみを取得したい場合に使う。
+        /// エンティティクラスには引数0のコンストラクターがないため、
+        /// 返すレコードがない場合はnullを返却している。
+        /// </remarks>
         internal static T QuerySingle<T>(
             string sql,
             SQLiteParameter[] parameters,
             Func<SQLiteDataReader, T> createEntity,
             T nullEntity)
         {
-            var result = new List<T>();
-
             using (var connection = new SQLiteConnection(SQLiteHelper.GetConnectionString()))
             using (var command = new SQLiteCommand(sql, connection))
             {
@@ -88,6 +116,17 @@ namespace SalaryManager.Infrastructure.SQLite
             return nullEntity;
         }
 
+        /// <summary>
+        /// クエリ実行
+        /// </summary>
+        /// <typeparam name="T">型パラメーター</typeparam>
+        /// <param name="sql">SQL文</param>
+        /// <param name="parameters">パラメーター</param>
+        /// <param name="createEntity">読み込まれるエンティティ</param>
+        /// <returns>エンティティ</returns>
+        /// <remarks>
+        /// null安全版。なるべくnullEntityありを使いたいところ。
+        /// </remarks>
         internal static IReadOnlyList<T> QueryPlural<T>(
             string sql,
             SQLiteParameter[] parameters,
@@ -116,7 +155,14 @@ namespace SalaryManager.Infrastructure.SQLite
             return result;
         }
 
-        [Obsolete("Transaction版を使うこと。")]
+        /// <summary>
+        /// 実行
+        /// </summary>
+        /// <param name="sql">SQL文</param>
+        /// <param name="parameters">パラメーター</param>
+        /// <remarks>
+        /// SQLを単体実行したい時に使う。なるべくTransaction版を検討すること。
+        /// </remarks>
         internal static void Execute(
         string sql,
         SQLiteParameter[] parameters
@@ -135,6 +181,15 @@ namespace SalaryManager.Infrastructure.SQLite
             }
         }
 
+        /// <summary>
+        /// 実行
+        /// </summary>
+        /// <param name="insert">Insert文</param>
+        /// <param name="update">Update文</param>
+        /// <param name="parameters">パラメーター</param>
+        /// <remarks>
+        /// Update文が実行できなかった(対応する行がない)場合にInsert文を実行する。
+        /// </remarks>
         internal static void Execute(
             string insert,
             string update,
