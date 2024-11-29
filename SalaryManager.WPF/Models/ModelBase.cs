56c7908153e91b3636064d6c4ab51cab1cece609
アルピーノ <77906739+Arpino9@users.noreply.github.com>
2024-11-29 23:05:44 +0900

    Update ModelBase.cs
    .net framework 4.8から.NET8に移行

--- SalaryManager.WPF/Models/ModelBase.cs
+++ SalaryManager.WPF/Models/ModelBase.cs
@@ -1,4 +1,4 @@
-﻿namespace SalaryManager.WPF.Models;
+namespace SalaryManager.WPF.Models;
 
 /// <summary>
 /// Model - 基底
@@ -8,4 +8,41 @@ public abstract class ModelBase<T> where T : class
 {
     /// <summary> ViewModel </summary>
     internal abstract T ViewModel { get; set; }
+
+    /// <summary>
+    /// SixLabors.Fonts.FontFamily を System.Windows.Media.FontFamily に変換
+    /// </summary>
+    /// <param name="sixLaborsFontFamily">SixLabors.Fonts.FontFamily</param>
+    /// <returns>System.Windows.Media.FontFamily</returns>
+    public FontFamily ConvertToWpfFontFamily(SixLabors.Fonts.FontFamily sixLaborsFontFamily)
+    {
+        // フォント名を取得
+        string fontName = sixLaborsFontFamily.Name;
+
+        try
+        {
+            // WPF の FontFamily を作成
+            return new FontFamily(fontName);
+        }
+        catch (ArgumentException)
+        {
+            // フォントが見つからない場合はデフォルトフォントを返す
+            return new FontFamily("Segoe UI");
+        }
+    }
+
+    /// <summary>
+    /// System.Drawing.Color を SolidColorBrush に変換
+    /// </summary>
+    /// <param name="drawingColor">System.Drawing.Color</param>
+    /// <returns>SolidColorBrush</returns>
+    public SolidColorBrush ConvertToBrush(System.Drawing.Color drawingColor)
+    {
+        return new SolidColorBrush(Color.FromArgb(
+            drawingColor.A, // Alpha
+            drawingColor.R, // Red
+            drawingColor.G, // Green
+            drawingColor.B  // Blue
+        ));
+    }
 }
