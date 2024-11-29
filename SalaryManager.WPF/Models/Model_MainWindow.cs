05dc8fa60c9645a65dc6c6f33b24bd764194dc12
アルピーノ <77906739+Arpino9@users.noreply.github.com>
2024-11-29 23:11:26 +0900

    Update Model_MainWindow.cs
    .net framework 4.8から.NET8に移行

--- SalaryManager.WPF/Models/Model_MainWindow.cs
+++ SalaryManager.WPF/Models/Model_MainWindow.cs
@@ -1,4 +1,4 @@
-﻿using Message = SalaryManager.Domain.Modules.Logics.Message;
+using Message = SalaryManager.Domain.Modules.Logics.Message;
 
 namespace SalaryManager.WPF.Models;
 
@@ -103,9 +103,9 @@ public class Model_MainWindow : ModelBase<ViewModel_MainWindow>
     /// </summary>
     internal void Window_Activated()
     {
-        this.ViewModel.Window_FontFamily.Value = XMLLoader.FetchFontFamily();
+        this.ViewModel.Window_FontFamily.Value = base.ConvertToWpfFontFamily(XMLLoader.FetchFontFamily());
         this.ViewModel.Window_FontSize.Value   = XMLLoader.FetchFontSize();
-        this.ViewModel.Window_Background.Value = XMLLoader.FetchBackgroundColorBrush();
+        this.ViewModel.Window_Background.Value = base.ConvertToBrush(XMLLoader.FetchBackgroundColorBrush());
     }
 
     #endregion
