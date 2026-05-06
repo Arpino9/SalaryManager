81dbfb0ddb25a902840a0c9910ff168ddc13bf42
okaji <okaji@OKAJIMA>
2026-05-06 20:20:11 +0900

    スピンボタンのPrism化

---
+++ SalaryManager.Prism/ViewModels/NumericUpDownViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalaryManager.Prism.ViewModels;
+
+public class NumericUpDownViewModel : BindableBase
+{
+    public NumericUpDownViewModel()
+    {
+
+    }
+}
